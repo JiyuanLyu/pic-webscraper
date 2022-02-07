3b8b1b995adabf08b6a311152fb8a562e4c42265
JiyuanLyu <chelsealyu0425@ucla.edu>
2022-02-06 20:07:50 -0800

    parse() function

--- IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
+++ IMDB_scraper/IMDB_scraper/spiders/imdb_spider.py
@@ -7,3 +7,18 @@ class ImdbSpider(scrapy.Spider):
     name = 'imdb_spider'
     
     start_urls = ['https://www.imdb.com/title/tt0241527/']
+
+    def parse(self, response):
+        """
+        This function navigates to the `Cast & Crew` page
+        from the starting movie page, and then calls the function
+        `parse_full_credits(self, response)`.
+        """
+
+        # Navigates to the `Cast & Crew` page
+        castcrew = response.urljoin("fullcredits")
+
+        # Call function in the `callback` argument to 
+        # a yielded `scrapy.Request`
+        yield scrapy.Request(castcrew,
+                             callback = self.parse_full_credits)
